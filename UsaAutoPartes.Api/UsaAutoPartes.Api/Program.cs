85236070045af7432fd7b0367e5ca4776075833c
Jaime Guzman <jaimeguzmanlt@gmail.com>
2026-05-08 10:57:47 -0400

    limite por peticion cambiado a 10000

--- UsaAutoPartes.Api/UsaAutoPartes.Api/Program.cs
+++ UsaAutoPartes.Api/UsaAutoPartes.Api/Program.cs
@@ -128,7 +128,9 @@ builder.Services.AddCors(X =>
         
 });
 
-builder.Services.AddGraphQLServer().ModifyRequestOptions(opt => opt.IncludeExceptionDetails = true)
+builder.Services.AddGraphQLServer()
+    .ModifyRequestOptions(opt => opt.IncludeExceptionDetails = true)
+    .ModifyCostOptions(options => options.MaxFieldCost = 10000)
     .AddQueryType(d => d.Name("Query"))
     .AddTypeExtension<ProductoQuery>()
     .AddTypeExtension<ProveedorQuery>()
